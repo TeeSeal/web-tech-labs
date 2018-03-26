23ba3ac5ab3b93cca61a69590b521120a4d1d279
TeeSeal <teesealz@gmail.com>
2018-03-26 10:00:50 +0300

    lab3: add initial files

---
+++ lab3/lib/lab/library/library.ex
@@ -0,0 +1,108 @@
+defmodule Lab.Library do
+  @moduledoc """
+  The Library context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Lab.Repo
+
+  alias Lab.Library.Tab
+
+  @doc """
+  Returns the list of tabs.
+
+  ## Examples
+
+      iex> list_tabs()
+      [%Tab{}, ...]
+
+  """
+  def list_tabs do
+    Repo.all(Tab)
+  end
+
+  def list_tabs_of_type(type) do
+    Repo.all(from tab in Tab, where: tab.type == ^type)
+  end
+
+  @doc """
+  Gets a single tab.
+
+  Raises `Ecto.NoResultsError` if the Tab does not exist.
+
+  ## Examples
+
+      iex> get_tab!(123)
+      %Tab{}
+
+      iex> get_tab!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_tab!(id), do: Repo.get!(Tab, id)
+
+  @doc """
+  Creates a tab.
+
+  ## Examples
+
+      iex> create_tab(%{field: value})
+      {:ok, %Tab{}}
+
+      iex> create_tab(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_tab(attrs \\ %{}) do
+    %Tab{}
+    |> Tab.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a tab.
+
+  ## Examples
+
+      iex> update_tab(tab, %{field: new_value})
+      {:ok, %Tab{}}
+
+      iex> update_tab(tab, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_tab(%Tab{} = tab, attrs) do
+    tab
+    |> Tab.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Tab.
+
+  ## Examples
+
+      iex> delete_tab(tab)
+      {:ok, %Tab{}}
+
+      iex> delete_tab(tab)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_tab(%Tab{} = tab) do
+    Repo.delete(tab)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking tab changes.
+
+  ## Examples
+
+      iex> change_tab(tab)
+      %Ecto.Changeset{source: %Tab{}}
+
+  """
+  def change_tab(%Tab{} = tab) do
+    Tab.changeset(tab, %{})
+  end
+end
---
+++ lab3/lib/lab_web/router.ex
@@ -0,0 +1,30 @@
+defmodule LabWeb.Router do
+  use LabWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", LabWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "/tabs/list/:type", TabController, :index
+    get "/tabs/new/:type", TabController, :new
+    resources "/tabs", TabController
+    resources "/users", UserController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", LabWeb do
+  #   pipe_through :api
+  # end
+end
